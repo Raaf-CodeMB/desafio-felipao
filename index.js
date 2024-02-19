e6d54461fd0a21939c6de6ceaea62aa3ede14f0a
Raaf-CodeMB <rafa.engmec2023@gmail.com>
2024-02-19 12:29:16 -0300

    commit2

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
 console.log ("desafioFelipao")
+
+// Definindo variáveis
+let nomeDoHeroi = "hulkMagrelo"
+let experienciaDoHeroi = 8657; 
+
+// Utilizando estrutura de decisão
+let nivel;
+if (experienciaDoHeroi < 1000) {
+    nivel = "Ferro";
+} else if (experienciaDoHeroi >= 1001 && experienciaDoHeroi <= 2000) {
+    nivel = "Bronze";
+} else if (experienciaDoHeroi >= 2001 && experienciaDoHeroi <= 5000) {
+    nivel = "Prata";
+} else if (experienciaDoHeroi >= 5001 && experienciaDoHeroi <= 7000) {
+    nivel = "Ouro";
+} else if (experienciaDoHeroi >= 7001 && experienciaDoHeroi <= 8000) {
+    nivel = "Platina";
+} else if (experienciaDoHeroi >= 8001 && experienciaDoHeroi <= 9000) {
+    nivel = "Ascendente";
+} else if (experienciaDoHeroi >= 9001 && experienciaDoHeroi <= 10000) {
+    nivel = "Imortal";
+} else {
+    nivel = "Radiante";
+}
+
+// Exibindo mensagem
+console.log(`O Herói de nome ${nomeDoHeroi} está no nível de ${nivel}`);
